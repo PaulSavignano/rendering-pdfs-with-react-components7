0f50e5c9b91f1e2204214368f0d4e292e1f9f6c9
Paul Savignano <Paul.Savignano@gmail.com>
2016-08-27 16:34:09 -0700

    Added PDFs to public navigation.

--- imports/ui/components/public-navigation.js
+++ imports/ui/components/public-navigation.js
@@ -3,12 +3,19 @@ import { LinkContainer } from 'react-router-bootstrap';
 import { Nav, NavItem } from 'react-bootstrap';
 
 export const PublicNavigation = () => (
-  <Nav pullRight>
-    <LinkContainer to="signup">
-      <NavItem eventKey={ 1 } href="/signup">Sign Up</NavItem>
-    </LinkContainer>
-    <LinkContainer to="login">
-      <NavItem eventKey={ 2 } href="/login">Log In</NavItem>
-    </LinkContainer>
-  </Nav>
+  <div>
+    <Nav>
+      <LinkContainer to="/pdfs">
+        <NavItem eventKey={ 1 } href="/documents">PDFs</NavItem>
+      </LinkContainer>
+    </Nav>
+    <Nav pullRight>
+      <LinkContainer to="signup">
+        <NavItem eventKey={ 2 } href="/signup">Sign Up</NavItem>
+      </LinkContainer>
+      <LinkContainer to="login">
+        <NavItem eventKey={ 3 } href="/login">Log In</NavItem>
+      </LinkContainer>
+    </Nav>
+  </div>
 );
